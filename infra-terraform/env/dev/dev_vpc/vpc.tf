13d73590da84b2d7da1c5e4824a4cf62f8da2d49
Divya120812 <divyackumar08@gmail.com>
2025-10-08 14:50:43 +0530

    Create vpc.tf

---
+++ infra-terraform/env/dev/dev_vpc/vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source        = "../../modules/vpc"
+  environment   = var.environment
+  vpc_cidr      = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  tags          = var.tags
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
