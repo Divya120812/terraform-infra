d216036041e739d94fd02f4ff26f6d03acc364b9
Divya120812 <divyackumar08@gmail.com>
2025-10-08 18:27:36 +0530

    Update main.tf

--- infra-terraform/modules/ec2/main.tf
+++ infra-terraform/modules/ec2/main.tf
@@ -2,8 +2,17 @@
 data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
-  filter { name = "name", values = ["al2023-ami-*-x86_64"] }
-  filter { name = "virtualization-type", values = ["hvm"] }
+  
+filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  
+filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
 }
 
 locals {
@@ -14,21 +23,41 @@ resource "aws_security_group" "ssh_http" {
   name   = "sg-ssh-http-${var.environment}"
   vpc_id = data.aws_subnet.selected.vpc_id
 
-  ingress { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = [var.allow_ssh_cidr] }
-  ingress { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+  
+ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allow_ssh_cidr]
+  }
 
-  egress  { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+     from_port = 80 
+     to_port   = 80
+     protocol = "tcp" 
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress  {
+     from_port = 0 
+     to_port = 0 
+     protocol = "-1"
+     cidr_blocks = ["0.0.0.0/0"] 
+  }
   tags = local.tags
+ }
+
+data "aws_subnet" "selected" {
+    id = var.subnet_id 
 }
 
-data "aws_subnet" "selected" { id = var.subnet_id }
+resource "aws_instance" "ec2" {
+  ami           = data.aws_ami.al2023.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-resource "aws_instance" "web" {
-  ami                         = data.aws_ami.al2023.id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [aws_security_group.ssh_http.id]
-  associate_public_ip_address = true
-  key_name                    = var.key_name
-  tags = merge(local.tags, { Name = "ec2-${var.environment}" })
+  tags = merge(var.tags, { Name = "${var.environment}-ec2" })
 }
