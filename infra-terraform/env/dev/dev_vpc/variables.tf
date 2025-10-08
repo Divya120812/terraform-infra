43726ffd33e8fa15283fc066498a71ef278448ff
Divya120812 <divyackumar08@gmail.com>
2025-10-08 16:52:16 +0530

    Create variables.tf

---
+++ infra-terraform/env/dev/dev_vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+}
