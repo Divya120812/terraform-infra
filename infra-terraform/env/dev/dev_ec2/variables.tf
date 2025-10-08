b3d62b8eec2b0b932a0971b25fccbea431a8d4ee
Divya120812 <divyackumar08@gmail.com>
2025-10-08 18:48:29 +0530

    Update variables.tf

--- infra-terraform/env/dev/dev_ec2/variables.tf
+++ infra-terraform/env/dev/dev_ec2/variables.tf
@@ -1,6 +1,32 @@
-variable "environment"     { type = string }
-variable "subnet_id"       { type = string }
-variable "instance_type"   { type = string }
-variable "allow_ssh_cidr"  { type = string, default = "0.0.0.0/0" }
-variable "key_name"        { type = string, default = null }
-variable "tags"            { type = map(string), default = {} }
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be launched"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "allow_ssh_cidr" {
+  description = "CIDR block allowed for SSH access"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags for EC2 resources"
+  type        = map(string)
+  default     = {}
+}
