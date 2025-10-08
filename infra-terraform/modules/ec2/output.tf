f760f129ce8f64b7db1ccf8407151cbaf2dce21a
Divya120812 <divyackumar08@gmail.com>
2025-10-08 18:45:36 +0530

    Update output.tf

--- infra-terraform/modules/ec2/output.tf
+++ infra-terraform/modules/ec2/output.tf
@@ -1,2 +1,14 @@
-output "instance_id" { value = aws_instance.web.id }
-output "public_ip"   { value = aws_instance.web.public_ip }
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "ec2_private_ip" {
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.ec2.private_ip
+}
