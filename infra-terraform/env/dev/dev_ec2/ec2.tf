73957c24e8d901034559a3960e533e90330f0118
Divya120812 <divyackumar08@gmail.com>
2025-10-08 14:48:47 +0530

    Update ec2.tf

--- infra-terraform/env/dev/dev_ec2/ec2.tf
+++ infra-terraform/env/dev/dev_ec2/ec2.tf
@@ -1,1 +1,12 @@
+module "ec2" {
+  source         = "../../modules/ec2"
+  environment    = var.environment
+  subnet_id      = module.vpc.public_subnet_ids[0]
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  allow_ssh_cidr = var.allow_ssh_cidr
+  tags           = var.tags
+}
 
+output "instance_id" { value = module.ec2.instance_id }
+output "public_ip"   { value = module.ec2.public_ip }
