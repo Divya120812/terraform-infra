b76877a0e8dc2b8ce643e9ea84f34a81da405f33
Divya120812 <divyackumar08@gmail.com>
2025-10-09 12:54:13 +0530

    Update main.tf

--- infra-terraform/modules/ec2/main.tf
+++ infra-terraform/modules/ec2/main.tf
@@ -40,14 +40,14 @@ resource "aws_security_group" "ec2_sg" {
 
 # Get subnet details
 data "aws_subnet" "selected" {
-  id = var.subnet_id
+  id = var.public_subnet_id  
 }
 
 # EC2 Instance
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.al2023.id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  subnet_id     = var.public_subnet_id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
